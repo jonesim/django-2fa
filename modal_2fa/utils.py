76a844f5b52a1ee9480f7fb0d29a4b48c123de39
jonesim <ian@cobi.co.uk>
2021-09-02 17:49:03 +0100

    Move menu from utils.py

--- modal_2fa/utils.py
+++ modal_2fa/utils.py
@@ -1,4 +1,3 @@
-from django_menus.menu import MenuItem, DividerItem
 from django.conf import settings
 from django.utils.module_loading import import_string
 
@@ -10,25 +9,6 @@ def get_client_ip_address(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
-
-
-def add_auth_menu(view):
-
-    if view.request.user.is_authenticated:
-        if view.request.session.get('authentication_method') in ['2fa', 'cookie']:
-            dropdown = [('remove_2fa', 'Remove 2FA'), ('change_2fa', 'Change 2FA'),
-                        ('user_devices', 'Authorised devices')]
-        else:
-            dropdown = [('auth_2fa', 'Add 2FA')]
-
-        dropdown += [('change_password', 'Change password'), DividerItem(), 'logout']
-        view.add_menu('user_menu').add_items(
-            MenuItem(menu_display=view.request.user, dropdown=dropdown, placement='bottom-end'),
-        )
-    else:
-        view.add_menu('user_menu', 'button_group').add_items(
-            MenuItem('login_modal', menu_display='Sign In', css_classes='btn-primary'),
-        )
 
 
 def get_custom_auth():
